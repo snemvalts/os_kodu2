86af45985c6f7e9a5318c6cde2044785d3f03312
Sander Nemvalts <snemvalts@gmail.com>
2019-11-06 19:22:35 +0200

    done

---
+++ precache-manifest.7f155c7f88f984844d3de8e3c3533f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78347414abd6c2398d055025afccb946",
+    "url": "/os_kodu2/index.html"
+  },
+  {
+    "revision": "ba11fd68242cb10d137c",
+    "url": "/os_kodu2/static/css/main.a88db1dc.chunk.css"
+  },
+  {
+    "revision": "097a3fbc5b5336bde4f9",
+    "url": "/os_kodu2/static/js/2.5a826f93.chunk.js"
+  },
+  {
+    "revision": "ba11fd68242cb10d137c",
+    "url": "/os_kodu2/static/js/main.6f1ca7a5.chunk.js"
+  },
+  {
+    "revision": "2ca1aecda884a661e92b",
+    "url": "/os_kodu2/static/js/runtime-main.ce90fcac.js"
+  }
+]);
